55f9f17e3afd49f4dfbcc36c56c29abab8549938
Dino Jakir <dino.jakir@eucon.com>
2025-12-10 13:59:00 +0100

    Configuration for angular material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { EventsListComponent } from './pages/events-list/events-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '**', component: EventsListComponent },
+];
